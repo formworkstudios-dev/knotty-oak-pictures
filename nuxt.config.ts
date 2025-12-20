ac25ef0ee059f2bf0de37056bdbb168d9b788cac
Michael Synan <hello@michaelsynan.com>
2025-12-20 10:04:13 -0500

    Feat: trying to deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-// deploying
+// deploying 2
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
